7ec34498e64947515c3dee7b0da7adf624a3baa4
holadmex <ojewumidimeji@gmail.com>
2025-07-21 01:33:40 +0100

    pipeline/vm fix

--- terraform/compute/output.tf
+++ terraform/compute/output.tf
@@ -1,10 +1,5 @@
 # compute/outputs.tf
 
-output "ssh_private_key" {
-  value     = tls_private_key.vm_ssh.private_key_pem
-  sensitive = true
-}
-
 output "vm_public_ip" {
   value = azurerm_public_ip.vm_dev_ip.ip_address
 }
